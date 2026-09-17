8377a7f5c3d0c99583b80f5873a243928c634399
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2026-09-17 09:56:06 +0300

    pagination: fix the response for an invalid page
    When the page value is invalid, return an empty result with the real
    count (page number out of range) or a 400 (page not a number),
    instead of the same bare 404 either way, so the client can tell which
    case it hit.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/core/pagination.py
+++ bublik/core/pagination.py
@@ -3,6 +3,8 @@
 
 from collections import OrderedDict
 
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from rest_framework.exceptions import ValidationError
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -12,7 +14,29 @@ class DefaultPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
+    def paginate_queryset(self, queryset, request, view=None):
+        self.request = request
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = self.get_page_number(request, paginator)
+
+        try:
+            self.page = paginator.page(page_number)
+        except PageNotAnInteger as exc:
+            raise ValidationError({'page': [str(exc)]}) from exc
+        except EmptyPage:
+            self.page = None
+            self._count = paginator.count
+            return []
+
+        return list(self.page)
+
     def get_pagination(self):
+        if self.page is None:
+            return OrderedDict([('count', self._count), ('next', None), ('previous', None)])
         return OrderedDict(
             [
                 ('count', self.page.paginator.count),
